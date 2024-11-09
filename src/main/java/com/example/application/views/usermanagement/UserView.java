7beb40e4b705bf2b2a14f45cfe4067053a36cc8f
Pavel Jetensky <pavel.jetensky@plus4u.net>
2024-11-09 19:38:46 +0100

    Simplification of Person View to contain only firstName, dob and email

---
+++ src/main/java/com/example/application/views/usermanagement/UserView.java
@@ -0,0 +1,79 @@
+package com.example.application.views.usermanagement;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("User management")
+@Route("")
+@Menu(order = 1, icon = "line-awesome/svg/user.svg")
+public class UserView extends VerticalLayout {
+
+    H3 h3 = new H3();
+    FormLayout form = new FormLayout();
+    TextField firstName = new TextField();
+    DatePicker dateOfBirth = new DatePicker();
+    EmailField email = new EmailField();
+    HorizontalLayout buttonsRow = new HorizontalLayout();
+    Button registerNewAccountBtn = new Button();
+    Button cancelBtn = new Button();
+
+    public UserView() {
+        setWidth("100%");
+        setMaxWidth("800px");
+        setHeight("min-content");
+
+        initUserForm();
+        add(form);
+
+        initButtonsRow();
+        add(buttonsRow);
+
+    }
+
+    private void initButtonsRow() {
+        buttonsRow.addClassName(Gap.MEDIUM);
+        buttonsRow.setWidth("100%");
+        registerNewAccountBtn.setText("Register new account");
+        registerNewAccountBtn.setWidth("min-content");
+        registerNewAccountBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        registerNewAccountBtn.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                Notification.show("User " + firstName.getValue() + " has been registered");
+            }
+        });
+        cancelBtn.setText("Cancel");
+        cancelBtn.setWidth("min-content");
+        buttonsRow.add(registerNewAccountBtn);
+        buttonsRow.add(cancelBtn);
+    }
+
+    private void initUserForm() {
+        h3.setText("Add new user");
+        h3.setWidth("100%");
+        add(h3);
+        form.setWidth("100%");
+        firstName.setLabel("First Name");
+        dateOfBirth.setLabel("Birthday");
+        email.setLabel("Email");
+        form.add(firstName);
+        form.add(dateOfBirth);
+        form.add(email);
+    }
+
+
+}
